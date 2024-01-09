dd6478b022ea6c3cc30b3cad2fb84ff9d06e9da3
Hidetaro7 <hidetaro7@gmail.com>
2024-01-09 22:46:24 +0900

    feat: first commit

--- src/code/code.ts
+++ src/code/code.ts
@@ -9,7 +9,7 @@ import { getSpacing } from "./modules/module";
 // full browser environment (see documentation).
 
 // This shows the HTML page in "ui.html".
-figma.showUI(__html__);
+figma.showUI(__html__, { width: 780, height: 700 });
 
 // Calls to "parent.postMessage" from within the HTML page will trigger this
 // callback. The callback will be passed the "pluginMessage" property of the
--- src/ui/components/sample-component.vue
+++ src/ui/components/sample-component.vue
@@ -1,23 +1,24 @@
 <script lang="ts" setup>
-  // data
-  const title = "Rectangle Creator";
-  let count = 5;
-
-  // methods
-  const create = () => {
-    window.parent.postMessage({pluginMessage: { type: "create-rectangles", count }}, "*")
-  }
-  const cancel = () => {
-    window.parent.postMessage({pluginMessage: { type: "cancel" }}, "*")
-  }
+// data
+const title = "Chikuwa";
+let count = 5;
+// methods
+const create = () => {
+  window.parent.postMessage(
+    { pluginMessage: { type: "create-rectangles", count } },
+    "*"
+  );
+};
+const cancel = () => {
+  window.parent.postMessage({ pluginMessage: { type: "cancel" } }, "*");
+};
 </script>
 
 <template>
   <h2>{{ title }}</h2>
-  <p>Count: <input id="count" v-model="count"></p>
+  <p>Count: <input id="count" v-model="count" /></p>
   <button id="create" @click="create()">Create</button>
   <button id="cancel" @click="cancel()">Cancel</button>
 </template>
 
-<style lang="scss">
-</style>
+<style lang="scss"></style>
